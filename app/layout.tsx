e02f001a2f3d80fcf4d6a7f715c2e96830e920f8
fiqriardiansyah <fiqriardian92@gmail.com>
2023-07-05 16:11:19 +0700

    fix: delete metadata index

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,13 @@ import Footer from "@/components/footer";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import "../styles/globals.css";
 
-export const metadata = {
-    title: "JSL e-Catalogue",
-    description: "JSL e-catalogue, cari product JSL sesuai dengan jenis mobil",
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <html lang="en" className="bg-slate-50">
                 <head>
+                    <title>JSL e-Catalogue</title>
+                    <meta name="description" content="JSL e-catalogue, cari product JSL sesuai dengan jenis mobil" />
                     <link rel="icon" type="image" href="/favicon.png"></link>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
